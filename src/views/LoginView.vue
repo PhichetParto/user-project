efaed674ba7974dacee18c8df56f2881ea6a471d
Phichet Parto <64160261@go.buu.ac.th>
2022-12-24 19:46:41 +0700

    3.LoginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { VForm } from "vuetify/components";
+const loginname = ref("");
+const password = ref("");
+const valid = ref(true);
+const form = ref<InstanceType<typeof VForm> | null>(null);
+const login = async () => {
+  const { valid } = await form.value!.validate();
+  if (valid) {
+    console.log("Form Success");
+  }
+};
+const reset = () => {
+  form.value?.reset();
+};
+</script>
+
+<template>
+  <v-app>
+    <v-main class="pa-5 bg-blue-grey-darken-2">
+      <v-card width="400px" class="mx-auto">
+        <v-card-title
+          primary-title
+          class="text-center text-h6 font-weight-bold"
+        >
+          Login
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid">
+            <v-text-field
+              label="Login Name"
+              v-model="loginname"
+              :rules="[
+                (v) => !!v || 'Login Name is required',
+                (v) =>
+                  v.length >= 4 ||
+                  'Name must be more than or equal 4 characters',
+              ]"
+              required
+            >
+            </v-text-field>
+            <v-text-field
+              label="Password"
+              type="password"
+              v-model="password"
+              :rules="[
+                (v) => !!v || 'Password is required',
+                (v) =>
+                  v.length >= 8 ||
+                  'Password must be more than or equal 8 characters',
+              ]"
+              required
+            ></v-text-field>
+          </v-form>
+          <pre>
+            {{ loginname }}
+            {{ password }}
+          </pre>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn color="success" @click="login">Login</v-btn>
+          <v-btn color="error" @click="reset">Clear</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
